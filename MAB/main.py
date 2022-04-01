587cec183b52f7d5c4a95ebea1509fbabf79be2a
mwkim0210 <81286336+mwkim0210@users.noreply.github.com>
2022-04-01 15:15:50 +0900

    changed n_experiments, ignore reference codes

--- MAB/main.py
+++ MAB/main.py
@@ -5,7 +5,7 @@ import bandit
 
 bandit = bandit.F1Bandit()
 n_trials = 2500
-n_experiments = 500
+n_experiments = 2500
 
 # This is an example.
 agents = [agent.Agent(bandit, policy.RandomPolicy()),

98bccb9aba6f74603b73d69582b9b3ff552c79c1
mwkim0210 <81286336+mwkim0210@users.noreply.github.com>
2022-04-21 23:01:13 +0900

    reference code for dp_functions

---
+++ DP/dp_ref.py
@@ -0,0 +1,233 @@
+import numpy as np
+from gridworld import GridworldEnv
+import time
+
+
+def policy_eval(policy, env, discount_factor=1.0, theta=1e-8):
+    """
+    Evaluate a policy given an environment and a full description of the environment's dynamics.
+
+    Args:
+        policy: [S, A] shaped matrix representing the policy.
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment.
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+
+    Returns:
+        Vector of length env.nS representing the value function.
+    """
+    # Start with a random (all 0) value function
+    V = np.zeros(env.nS)
+    while True:
+        delta = 0
+        # For each state, perform a "full backup"
+        for s in range(env.nS):
+            v = 0
+            # Look at the possible next actions
+            for a, action_prob in enumerate(policy[s]):
+                # For each action, look at the possible next states...
+                for prob, next_state, reward, done in env.P[s][a]:
+                    # Calculate the expected value. Ref: Sutton book eq. 4.6.
+                    v += action_prob * prob * (reward + discount_factor * V[next_state])
+            # How much our value function changed (across any states)
+            delta = max(delta, np.abs(v - V[s]))
+            V[s] = v
+        # Stop evaluating once our value function change is below a threshold
+        if delta < theta:
+            break
+    return np.array(V)
+
+
+def policy_iter(env, policy_eval_fn=policy_eval, discount_factor=1.0):
+    """
+    Policy Improvement Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+
+    Args:
+        env: The OpenAI environment.
+        policy_eval_fn: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: gamma discount factor.
+
+    Returns:
+        A tuple (policy, V).
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+
+    """
+
+    def one_step_lookahead(state, V):
+        """
+        Helper function to calculate the value for all action in a given state.
+
+        Args:
+            state: The state to consider (int)
+            V: The value to use as an estimator, Vector of length env.nS
+
+        Returns:
+            A vector of length env.nA containing the expected value of each action.
+        """
+        A = np.zeros(env.nA)
+        for a in range(env.nA):
+            for prob, next_state, reward, done in env.P[state][a]:
+                A[a] += prob * (reward + discount_factor * V[next_state])
+        return A
+
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+
+    while True:
+        # Evaluate the current policy
+        V = policy_eval_fn(policy, env, discount_factor)
+
+        # Will be set to false if we make any changes to the policy
+        policy_stable = True
+
+        # For each state...
+        for s in range(env.nS):
+            # The best action we would take under the current policy
+            chosen_a = np.argmax(policy[s])
+
+            # Find the best action by one-step lookahead
+            # Ties are resolved arbitarily
+            action_values = one_step_lookahead(s, V)
+            best_a = np.argmax(action_values)
+
+            # Greedily update the policy
+            if chosen_a != best_a:
+                policy_stable = False
+                # print(s)
+            policy[s] = np.eye(env.nA)[best_a]
+
+        # If the policy is stable we've found an optimal policy. Return it
+        if policy_stable:
+            return policy, V
+
+
+def value_iteration(env, theta=0.0001, discount_factor=1.0):
+    """
+    Value Iteration Algorithm.
+
+    Args:
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment.
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+
+    Returns:
+        A tuple (policy, V) of the optimal policy and the optimal value function.
+    """
+
+    def one_step_lookahead(state, V):
+        """
+        Helper function to calculate the value for all action in a given state.
+
+        Args:
+            state: The state to consider (int)
+            V: The value to use as an estimator, Vector of length env.nS
+
+        Returns:
+            A vector of length env.nA containing the expected value of each action.
+        """
+        A = np.zeros(env.nA)
+        for a in range(env.nA):
+            for prob, next_state, reward, done in env.P[state][a]:
+                A[a] += prob * (reward + discount_factor * V[next_state])
+        return A
+
+    V = np.zeros(env.nS)
+    while True:
+        # Stopping condition
+        delta = 0
+        # Update each state...
+        for s in range(env.nS):
+            # Do a one-step lookahead to find the best action
+            A = one_step_lookahead(s, V)
+            best_action_value = np.max(A)
+            # Calculate delta across all states seen so far
+            delta = max(delta, np.abs(best_action_value - V[s]))
+            # Update the value function. Ref: Sutton book eq. 4.10.
+            V[s] = best_action_value
+            # Check if we can stop
+        if delta < theta:
+            break
+
+    # Create a deterministic policy using the optimal value function
+    policy = np.zeros([env.nS, env.nA])
+    for s in range(env.nS):
+        # One step lookahead to find the best action for this state
+        A = one_step_lookahead(s, V)
+        best_action = np.argmax(A)
+        # Always take the best action
+        policy[s, best_action] = 1.0
+
+    return policy, V
+
+
+def main():
+    discount_factor = 0.0
+
+    print('--------Policy evaluation--------')
+    env = GridworldEnv()
+    uniform_policy = np.ones([env.nS, env.nA]) / env.nA
+    v = policy_eval(uniform_policy, env, discount_factor)
+
+    v = v.reshape(env.shape)
+    print(v)
+
+    print('---------------------------------')
+
+    print('--------Policy iteration---------')
+
+    start = time.time()
+    policy, v = policy_iter(env, discount_factor=discount_factor)
+    policy_iter_time = time.time() - start
+
+    print("Policy Probability Distribution:")
+    print(policy)
+    print("")
+
+    print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+    print(np.reshape(np.argmax(policy, axis=1), env.shape))
+    print("")
+
+    print("Reshaped Grid Value Function:")
+    print(v.reshape(env.shape))
+
+    print("")
+    print('---------------------------------')
+
+    print('--------Value iteration---------')
+
+    start = time.time()
+    policy, v = value_iteration(env, discount_factor=discount_factor)
+    value_iter_time = time.time() - start
+
+    print("Policy Probability Distribution:")
+    print(policy)
+    print("")
+
+    print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+    print(np.reshape(np.argmax(policy, axis=1), env.shape))
+    print("")
+
+    np.set_printoptions(precision=2)
+
+    print("Reshaped Grid Value Function:")
+    print(v.reshape(env.shape))
+    print("")
+
+    print('---------------------------------')
+
+    print(f"Policy Iteration Converge time: {policy_iter_time}")
+    print(f"Value Iteration Converge time: {value_iter_time}")
+
+
+if __name__ == '__main__':
+    main()
